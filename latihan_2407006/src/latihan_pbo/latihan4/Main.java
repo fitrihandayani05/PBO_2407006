c594833a97e5e8a41fabffbdf8995d3619dd59ac
fitrihandayanihue <fitrihandayani161105@gmail.com>
2026-04-16 14:29:50 +0700

    polymorphism

--- latihan_2407006/src/latihan_pbo/latihan4/Main.java
+++ latihan_2407006/src/latihan_pbo/latihan4/Main.java
@@ -6,15 +6,15 @@ public class Main {
 
         System.out.println("--- DAFTAR PESANAN ---");
 
-        pesanan = new MenuItem("Nasi Goreng", 2, 12000);
+        pesanan = new MenuItem("Nasi Goreng", 4, 15000);
         pesanan.tampilkan();
         System.out.println("------------------------------");
 
-        pesanan = new MenuItem("Es Teh", 3, 3000);
+        pesanan = new MenuItem("Es Teh", 5, 8000);
         pesanan.tampilkan();
         System.out.println("------------------------------");
 
-        pesanan = new MenuItem("Mie Ayam", 3, 13000);
+        pesanan = new MenuItem("ketoprak", 3, 12000);
         pesanan.tampilkan();
         System.out.println("------------------------------");
     }
